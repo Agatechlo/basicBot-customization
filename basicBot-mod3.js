8717fa5e093925453a2afcc1a120fd0d942c0bad
Agatechlo <compname@verizon.net>
2015-05-06 20:58:07 -0700

    Update basicBot-mod3.js

--- basicBot-mod3.js
+++ basicBot-mod3.js
@@ -1155,7 +1155,8 @@
                 'hueh', 'hu3', 'brbr', 'heu', 'brbr', 'kkkk', 'spoder', 'mafia', 'zuera', 'zueira',
                 'zueria', 'aehoo', 'aheu', 'alguem', 'algum', 'brazil', 'zoeira', 'fuckadmins', 'affff', 'vaisefoder', 'huenaarea',
                 'hitler', 'ashua', 'ahsu', 'ashau', 'lulz', 'huehue', 'hue', 'huehuehue', 'merda', 'pqp', 'puta', 'mulher', 'pula', 'retarda', 'caralho', 'filha', 'ppk',
-                'gringo', 'fuder', 'foder', 'hua', 'ahue', 'modafuka', 'modafoka', 'mudafuka', 'mudafoka', 'ooooooooooooooo', 'foda'
+                'gringo', 'fuder', 'foder', 'hua', 'ahue', 'modafuka', 'modafoka', 'mudafuka', 'mudafoka', 'ooooooooooooooo', 'foda',
+                'fuck', 'fuk', 'fuq', 'dafuq', 'dufuq', 'feck', 'fick', 'fahk', 'fak', 'shit', 'shet', 'shitty', 'zibby'
             ],
             curses: [
                 'nigger', 'faggot', 'nigga', 'niqqa', 'motherfucker', 'modafocka'
